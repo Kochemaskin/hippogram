b0d0e882019f7b1df1ee6e67b5a95433a2a1bcd8
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-09-26 17:25:44 -0600

    feat(FollowingRepository, FollowingService) following service implemented

--- src/main/java/com/example/hippogram/repository/FollowingRepository.java
+++ src/main/java/com/example/hippogram/repository/FollowingRepository.java
@@ -1,10 +1,12 @@
 package com.example.hippogram.repository;
 
 import com.example.hippogram.entity.Following;
+import com.example.hippogram.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FollowingRepository extends JpaRepository<Following, Long> {
 
+    Following findByFollowerAndFollowing(User currentUser, User userToUnfollow);
 }
